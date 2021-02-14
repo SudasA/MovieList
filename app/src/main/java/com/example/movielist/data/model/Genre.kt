1cee360d6793c13dc80c624e33f778db93eb7211
sajka <Sudasa1989>
2021-02-14 16:24:09 +0300

    Gradle implem

--- app/src/main/java/com/example/movielist/data/model/Genre.kt
+++ app/src/main/java/com/example/movielist/data/model/Genre.kt
@@ -1,8 +1,6 @@
 package com.example.movielist.data.model
 
-data class Genre(
-    val id: Int,
-    val name: String) {
+data class Genre(val id: Int, val name: String) {
     override fun toString(): String {
         return name
     }
